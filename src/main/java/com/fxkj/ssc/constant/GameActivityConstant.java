42142918fabcad3b5cdfeb21fba488c744a10b0d
james <james@gail.com>
2019-10-25 11:50:59 +0800

    提交游戏活动常量

---
+++ src/main/java/com/fxkj/ssc/constant/GameActivityConstant.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.fxkj.ssc.constant;
+
+/**  
+* @ClassName: GameActivityConstant  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author james
+* @date 2019年10月25日  
+*    
+*/
+public class GameActivityConstant {
+
+	/** 大爆奖活动的消息*/
+	public static final Long brust_Activity = -10000L;
+}

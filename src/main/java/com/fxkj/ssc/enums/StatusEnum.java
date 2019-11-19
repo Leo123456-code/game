3806eabcc75336c35b6ae2f5ad552ac477571c0c
peck <xiataindf@163.com>
2019-11-19 09:44:52 +0800

    修改枚举

--- src/main/java/com/fxkj/ssc/enums/StatusEnum.java
+++ src/main/java/com/fxkj/ssc/enums/StatusEnum.java
@@ -17,8 +17,7 @@ public enum StatusEnum {
     JIN_YONG(3, "禁用"),
     DONG_JIE(4, "永久冻结"),
 	PENDING_REVIEW(5, "待审核"),
-	REVIEW_PASSED(6, "审核通过"),
-	AUDIT_FAILURE(7, "审核未通过"),
+	AUDIT_FAILURE(6, "审核未通过")
     ;
 
     private Integer code;

a299cac3188f55509de1638b95888ae46c9e19da
Karl <Karl@DESKTOP-VOK54Q0>
2019-11-27 17:46:59 +0800

    新加tostring方法

--- src/main/java/com/fxkj/ssc/message/BaseResult.java
+++ src/main/java/com/fxkj/ssc/message/BaseResult.java
@@ -7,38 +7,46 @@ package com.fxkj.ssc.message;
 * @author Jack  
 * @date 2017年8月18日 上午10:18:27 
 * @version V1.0   
-*/ 
+*/
 public class BaseResult {
 
     //错误码
-	private Integer       code    = 0;
+    private Integer code    = 0;
     //消息
-	private String        message = "成功";
+    private String  message = "成功";
     //数据对象
-	private Object       data;
+    private Object  data;
 
-	public Integer getCode() {
-		return code;
-	}
+    public Integer getCode() {
+        return code;
+    }
 
-	public void setCode(Integer code) {
-		this.code = code;
-	}
+    public void setCode(Integer code) {
+        this.code = code;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-	public Object getData() {
-		return data;
-	}
+    public Object getData() {
+        return data;
+    }
 
-	public void setData(Object data) {
-		this.data = data;
-	}
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    /** 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "BaseResult [code=" + code + ", message=" + message + ", data=" + data + "]";
+    }
 
 }

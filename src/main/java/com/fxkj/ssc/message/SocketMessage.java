bc84ed77149a6cfbc8844724e397afaaee2fb459
Jack <javagcsjack@gmail.com>
2019-12-03 18:15:25 +0800

    消息新增随机数

--- src/main/java/com/fxkj/ssc/message/SocketMessage.java
+++ src/main/java/com/fxkj/ssc/message/SocketMessage.java
@@ -1,11 +1,19 @@
 package com.fxkj.ssc.message;
 
-public class SocketMessage {
+import com.fxkj.ssc.utils.date.CreaditUuid;
 
+public class SocketMessage {
+    //消息随机id
+	private String messageId;
+	
 	//消息类型，用来做业务区分
 	private Integer messageType;
 	//消息内容
 	private BaseResult baseResult;
+	
+	public SocketMessage(){
+		this.messageId = CreaditUuid.getNumberUuid16();
+	}
 	
 	public BaseResult getBaseResult() {
 		return baseResult;
@@ -18,6 +26,12 @@ public class SocketMessage {
 	}
 	public void setMessageType(Integer messageType) {
 		this.messageType = messageType;
+	}
+	public String getMessageId() {
+		return messageId;
+	}
+	public void setMessageId(String messageId) {
+		this.messageId = messageId;
 	}
 	
 }

ae6324ca28c863c1c45f2ac18aba0453c0a8d747
Jack <javagcsjack@gmail.com>
2019-12-23 14:46:15 +0800

    工具类提交

--- src/main/java/com/fxkj/ssc/utils/date/DateHelper.java
+++ src/main/java/com/fxkj/ssc/utils/date/DateHelper.java
@@ -1,11 +1,15 @@
 package com.fxkj.ssc.utils.date;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import com.fxkj.ssc.utils.base.StringHelper;
+import org.apache.commons.lang.StringUtils;
 
 /**
  *   
@@ -334,4 +338,49 @@ public class DateHelper {
 		String dateTime = formatDate(date);
 		return formatDateTime(dateTime);
 	}
+	
+	 /**
+     * 获取两个日期之间的所有日期
+     * 
+     * @param startDate  开始日期
+     * @param endDate   结束日期
+     * @return  List集合
+     */
+
+	 /**
+	     * 获取两个日期之间的所有日期
+	     *
+	     * @param startTime
+	     *            开始日期
+	     * @param endTime
+	     *            结束日期
+	     * @return
+	     */
+	public static List<String> getDays(String startTime, String endTime) {
+
+		// 返回的日期集合
+		List<String> days = new ArrayList<String>();
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date start = dateFormat.parse(startTime);
+			Date end = dateFormat.parse(endTime);
+
+			Calendar tempStart = Calendar.getInstance();
+			tempStart.setTime(start);
+
+			Calendar tempEnd = Calendar.getInstance();
+			tempEnd.setTime(end);
+			tempEnd.add(Calendar.DATE, +1);// 日期加1(包含结束)
+			while (tempStart.before(tempEnd)) {
+				days.add(dateFormat.format(tempStart.getTime()));
+				tempStart.add(Calendar.DAY_OF_YEAR, 1);
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return days;
+	}
+	
 }
